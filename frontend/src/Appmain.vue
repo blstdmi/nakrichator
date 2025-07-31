45ae14858401caf189df608f662575fc8ed17958
lstdmi <79197205563@mail.ru>
2025-08-01 02:33:28 +0300

    head svg

---
+++ frontend/src/Appmain.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+<header>
+  <!-- <caption style="left:20%; top:0px;position: absolute; font-size: 10vw; pointer-events: none;">
+    <p style="transform: skew(15deg,-15deg);">НАКР</p>
+    <p style="transform: skew(15deg,-15deg);">И</p>
+    <p style="transform: skew(15deg,-15deg);">ЧАТОР</p></caption> -->
+    <caption>
+      <svg xmlns="http://www.w3.org/2000/svg"     width="90vw" height="50vh"  >
+
+
+     <text text-anchor="middle" x="35vw" y="7vw" font-family="Times New Roman" font-size="10vw" >
+        НАКРИЧАТОР
+      </text>
+
+      <path id="SunCatcherStudio" fill="none" stroke="#000000" d="M 32.550491,148.48008 A -108.15144,-108.15144 0 0 1 140.70194,40.328644 -108.15144,-108.15144 0 0 1 248.85338,148.48008 -108.15144,-108.15144 0 0 1 140.70194,256.63153 -108.15144,-108.15144 0 0 1 32.550491,148.48008 Z" />  
+<text font-size="30" fill="#000000" letter-spacing="2" font-family="sans-serif" font-weight="bold">
+<textPath xlink:href="#SunCatcherStudio" side="left" startOffset="5">НАКРИЧАТОР</textPath></text>
+
+<path id="SunCatcherStudio2" fill="none" stroke="none"         d="M 55.550491,148.48008 A -108.15144,-108.15144 0 0 1 140.70194,40.328644 -108.15144,-108.15144 0 0 1 248.85338,148.48008 -108.15144,-108.15144 0 0 1 140.70194,256.63153 -108.15144,-108.15144 0 0 1 32.550491,148.48008 Z"></path>
+<text font-size="30" fill="#000000" letter-spacing="2" font-family="sans-serif" font-weight="bold">
+  <textPath xlink:href="#SunCatcherStudio2" side="left" startOffset="5">YOUR&nbsp;TEXT&nbsp;HERE</textPath></text>
+  </svg>
+
+    </caption>
+</header>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+//import App from './App.vue'
+
+//createApp(App).mount('#app')
+
+
+
+import App from './Appmain.vue'
 
 createApp(App).mount('#app')
